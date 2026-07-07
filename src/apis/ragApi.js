9e7ad349a27ef41c950e81268b674064ce529bf7
황지혜 <jihye10226@naver.com>
2026-07-08 05:06:49 +0900

    챗봇 정보 수정 재시도

--- src/apis/ragApi.js
+++ src/apis/ragApi.js
@@ -1,12 +1,28 @@
+// import axiosInstance from "./axiosConfig";
+
+// export const askRag = async ({ message, storeSeq }) => {
+//   const response = await axiosInstance.get("/ai/chat", {
+//     params: {
+//       message,
+//       storeSeq,
+//       userSeq,
+//       userType
+//     },
+//   });
+
+//   return response.data.answer;
+// };
+
 import axiosInstance from "./axiosConfig";
 
-export const askRag = async ({ message, storeSeq }) => {
+// message, storeSeq뿐만 아니라 userSeq, userType도 같이 받아야 함
+export const askRag = async ({ message, storeSeq, userSeq, userType }) => {
   const response = await axiosInstance.get("/ai/chat", {
     params: {
       message,
       storeSeq,
       userSeq,
-      userType
+      userType,
     },
   });
 
